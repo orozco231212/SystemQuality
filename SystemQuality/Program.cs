893e90f49f0832d9286f87a00fcf6f85047c4a10
David Alejandro Orozco Henao <davidalejandroorozcohenao@gmail.com>
2026-03-09 15:14:26 -0500

    utilidad de pausa y configuracion de consola

--- SystemQuality/Program.cs
+++ SystemQuality/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using System;
+
+class Program
+{
+    static void Main() { ShowMainMenu(); }
+
+    static void ShowMainMenu()
+    {
+        Console.WriteLine("===INICIANDO SISTEMA BIBLIOTECA===");
+        Pause();
+    }
+
+    static void Pause()
+    {
+        Console.WriteLine("\nPresione una tecla para continuar...");
+        Console.ReadKey();
+    }
+}
