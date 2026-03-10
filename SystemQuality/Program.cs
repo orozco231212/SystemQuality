bcd70165426389eb880102a9876551d89e9b485a
David Alejandro Orozco Henao <davidalejandroorozcohenao@gmail.com>
2026-03-10 05:21:05 -0500

    Actualicion de errores en munu principal

--- SystemQuality/Program.cs
+++ SystemQuality/Program.cs
@@ -8,15 +8,4 @@ class Program
 
     static void Main() { ShowMainMenu(); }
 
-    static void ShowMainMenu()
-    {
-        Console.WriteLine("INICIANDO SISTEMA BIBLIOTECA");
-        Pause();
-    }
-
-    static void Pause()
-    {
-        Console.WriteLine("\nPresione una tecla para continuar...");
-        Console.ReadKey();
-    }
 }
